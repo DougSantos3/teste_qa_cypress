038a39e13011e862d8d0ef52c53a8f325ef9ae0e
douglas <douglasmartinssantos7@gmail.com>
2024-12-03 01:49:15 -0300

    new commands and refactor

--- cypress/support/commands/commons.js
+++ cypress/support/commands/commons.js
@@ -1,13 +1,13 @@
-import "cypress-xpath"
+import 'cypress-xpath'
 
-Cypress.Commands.add("login", (email, password) => {
-  cy.visit("/login")
+Cypress.Commands.add('login', (email, password) => {
+  cy.visit('/login')
   cy.get('[data-testid="email"]').type(email)
-  cy.get("senha").type(password)
+  cy.get('senha').type(password)
   cy.get('[data-testid="entrar"]').click()
 })
 
-Cypress.Commands.add("createUser", (email, password, clickCheckbox = false) => {
+Cypress.Commands.add('createUser', (email, password, clickCheckbox = false) => {
   cy.get('[data-testid="cadastrar"]').click()
   cy.get('[data-testid="nome"]').type(email)
   cy.get('[data-testid="email"]').type(password)
@@ -20,16 +20,17 @@ Cypress.Commands.add("createUser", (email, password, clickCheckbox = false) => {
   cy.get('[data-testid="cadastrarUsuario"]').click()
 })
 
-Cypress.Commands.add("createProduct", () => {
+Cypress.Commands.add('createProductUI', () => {
   const randomNum = Math.floor(Math.random() * 50000000) + 1
 
   const product = {
-    nome: `Produto ${randomNum}`, 
-    preco: (Math.random() * 100).toFixed(2), 
-    descricao: `Descrição do Produto ${randomNum}`, 
-    quantidade: Math.floor(Math.random() * 10) + 1, 
+    nome: `Produto ${randomNum}`, // Nome com número aleatório
+    preco: (Math.random() * 100).toFixed(2), // Preço aleatório entre 0 e 100
+    descricao: `Descrição do Produto ${randomNum}`, // Descrição com número aleatório
+    quantidade: Math.floor(Math.random() * 10) + 1, // Quantidade aleatória entre 1 e 10
   }
 
+  // Criação do produto
   cy.get('[data-testid="cadastrarProdutos"]').click()
   cy.get('[data-testid="nome"]').type(product.nome)
   cy.get('[data-testid="preco"]').type(product.preco)
@@ -44,9 +45,13 @@ Cypress.Commands.add('generateRandomEmail', () => {
   return `novo${randomNumber}email${randomNumber}@dominio.com`
 })
 
+Cypress.Commands.add('generateRandomProduct', () => {
+  const randomNumber = Math.floor(Math.random() * 50000000) + 1
+  return `Playstation 5 modelo-${randomNumber}`
+})
 
 //Not in use
-Cypress.Commands.add("mockRegisterProduct", () => {
+Cypress.Commands.add('mockRegisterProduct', () => {
   cy.intercept('POST', 'produtos', {
     statusCode: 201,
     body: {
