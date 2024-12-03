0fc25d7538e48feec16266ea5b89db4a0c430325
douglas <douglasmartinssantos7@gmail.com>
2024-12-03 01:49:57 -0300

    refactor cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,36 +1,36 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
-const cyPostgres = require("cypress-postgres-10v-compatibility");
+const cyPostgres = require('cypress-postgres-10v-compatibility');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      on("task", {
+      on('task', {
         dbQuery: (query) => cyPostgres(query.query, query.connection),
       });
 
       return config;
     },
-    baseUrl: "https://serverest.dev/",
+    baseUrl: 'https://serverest.dev/',
     env: {
-      apiKey: "Your api key",
-      token: "Your token",
-      cookie: "Your cookie",
-      boardId: "Your  board id",
+      apiKey: 'Your api key',
+      token: 'Your token',
+      cookie: 'Your cookie',
+      boardId: 'Your  board id',
     },
     retries: {
       runMode: 0,
       openMode: 0,
     },
-    specPattern: "cypress/e2e/**/tests/*.cy.{js,ts,jsx,tsx}",
+    specPattern: 'cypress/e2e/**/tests/*.cy.{js,ts,jsx,tsx}',
   },
 
   fixturesFolder: false,
 
   component: {
     devServer: {
-      framework: "react",
-      bundler: "vite",
+      framework: 'react',
+      bundler: 'vite',
     },
   },
 });
