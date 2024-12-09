618d6e7d707fa0e13e05141a896c19f97b4a955d
douglas <douglasmartinssantos7@gmail.com>
2024-12-09 19:40:37 -0300

    new request part 2

---
+++ cypress/e2e/api/requests/search_user.js
@@ -0,0 +1,11 @@
+export const searchProduct = (user_id) => {
+ 
+    cy.request({
+        method: "GET",
+        url: `usuarios/${user_id}`,
+        headers: {
+          Accept: "application/json",
+        },
+    })
+  }
+  
