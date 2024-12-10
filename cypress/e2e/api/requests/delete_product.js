2ed2ba31e12fc65dd8fdc0eff3806d90d1d919ba
douglas <douglasmartinssantos7@gmail.com>
2024-12-10 12:20:38 -0300

    refactor: improve function parameter formatting for readability

--- cypress/e2e/api/requests/delete_product.js
+++ cypress/e2e/api/requests/delete_product.js
@@ -1,13 +1,11 @@
 export const deleteProductRequest = (authorization, productId) => {
-   
   return cy.request({
-      method: 'DELETE',
-      url: `produtos/${productId}`,
-      headers: {
-        Accept: 'application/json',
-        Authorization: authorization,
-      },
-      failOnStatusCode: false,
-    })
-  }
-  
+    method: 'DELETE',
+    url: `produtos/${productId}`,
+    headers: {
+      Accept: 'application/json',
+      Authorization: authorization,
+    },
+    failOnStatusCode: false,
+  })
+}
