324ebad8fe19022bd2dddd504e533956b48bbaae
douglas <douglasmartinssantos7@gmail.com>
2024-12-10 11:57:01 -0300

    feat: add maximium value validation to quantidy field

--- cypress/e2e/api/contracts/allUserSchema.js
+++ cypress/e2e/api/contracts/allUserSchema.js
@@ -2,6 +2,6 @@ import Joi from 'joi'
 
 
 export const allUserSchema = Joi.object({
-  quantidade: Joi.number().integer().min(0).required(),
+  quantidade: Joi.number().integer().min(0).max(9007199254740991).required(),
   usuarios: Joi.array().items(Joi.object({})).min(0).required()
 })
