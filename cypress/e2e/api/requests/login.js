cbde7e1a920ea810689e856f7d2cdd276c834a63
douglas <douglasmartinssantos7@gmail.com>
2024-12-10 12:20:51 -0300

    refactor: improve function parameter formatting for readability

--- cypress/e2e/api/requests/login.js
+++ cypress/e2e/api/requests/login.js
@@ -1,18 +1,19 @@
 export const loginRequest = (email, password) => {
-
-    return cy.request({
-        method: 'POST',
-        url: 'login',
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json'
-        },
-        body: {
-            email,
-            password
-        }
-    }).then((response) => {
-        const authorization = response.body.authorization
-        cy.wrap(authorization).as('authorization')
+  return cy
+    .request({
+      method: 'POST',
+      url: 'login',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: {
+        email,
+        password,
+      },
+    })
+    .then((response) => {
+      const authorization = response.body.authorization
+      cy.wrap(authorization).as('authorization')
     })
 }
