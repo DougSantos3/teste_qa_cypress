f285a186156e419a1c3adc49c7385e701f6c9dba
douglas <douglasmartinssantos7@gmail.com>
2024-12-10 12:19:29 -0300

    refactor: improve function parameter formatting for readability

--- cypress/e2e/api/requests/create_product.js
+++ cypress/e2e/api/requests/create_product.js
@@ -1,24 +1,30 @@
-export const createProductRequest = (authorization, name, price, description, quantidy) => {
+export const createProductRequest = (
+  authorization,
+  name,
+  price,
+  description,
+  quantidy
+) => {
+  const requestBody = {
+    nome: name,
+    preco: price,
+    descricao: description,
+    quantidade: quantidy,
+  }
 
-    const requestBody = {
-        nome: name,
-        preco: price,
-        descricao: description,
-        quantidade: quantidy
-    }
-
-    return cy.request({
-        method: 'POST',
-        url: 'produtos',
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-            Authorization: authorization  
-        },
-        body: requestBody
-    }).then((response) => {
-        const productId = response.body._id  
-        cy.wrap(productId).as('productId')  
+  return cy
+    .request({
+      method: 'POST',
+      url: 'produtos',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: authorization,
+      },
+      body: requestBody,
+    })
+    .then((response) => {
+      const productId = response.body._id
+      cy.wrap(productId).as('productId')
     })
 }
-  
