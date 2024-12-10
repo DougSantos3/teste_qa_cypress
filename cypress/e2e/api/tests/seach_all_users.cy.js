ccf984cccb836c6cbd1b250a32a3b56007f0049c
douglas <douglasmartinssantos7@gmail.com>
2024-12-10 12:47:15 -0300

    styles(tests): format code for better readability

--- cypress/e2e/api/tests/seach_all_users.cy.js
+++ cypress/e2e/api/tests/seach_all_users.cy.js
@@ -1,14 +1,15 @@
-import { listAllRegisteredUsers } from "../requests/list_all_registered_users"
-import { allUserSchema } from "../contracts/allUserSchema"
+import { listAllRegisteredUsers } from '../requests/list_all_registered_users'
+import { allUserSchema } from '../contracts/allUserSchema'
 
-
-describe("Contract Test for empty user listing", () => {
-  it("List of users", () => {
+describe('Contract Test for empty user listing', () => {
+  it('List of users', () => {
     cy.generateRandomEmailAndPassword().then((userData) => {
-      listAllRegisteredUsers(userData.email, userData.password).should((response) => {
-        expect(response.status).to.be.eq(200)
-        return allUserSchema.validateAsync(response.body)
-      })
+      listAllRegisteredUsers(userData.email, userData.password).should(
+        (response) => {
+          expect(response.status).to.be.eq(200)
+          return allUserSchema.validateAsync(response.body)
+        }
+      )
     })
   })
 })
