f7c6e7004aef21a0dc57b4710ffd03ed01aaa7b7
douglas <douglasmartinssantos7@gmail.com>
2024-12-10 11:59:03 -0300

    feat: add minimum length validation to _id field

--- cypress/e2e/api/contracts/alterUserSchema.js
+++ cypress/e2e/api/contracts/alterUserSchema.js
@@ -3,5 +3,5 @@ import Joi from 'joi'
 
 export const alterUserSchema = Joi.object({
   message: Joi.string().required().equal('Registro alterado com sucesso'),
-  _id: Joi.string().required(),
+  _id: Joi.string().min(1).required(),
 })
