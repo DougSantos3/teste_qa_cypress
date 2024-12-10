2dd415e7ac069cd4166b28d0ac63b773d9647867
douglas <douglasmartinssantos7@gmail.com>
2024-12-10 12:20:27 -0300

    refactor: improve function parameter formatting for readability

--- cypress/e2e/api/requests/create_user.js
+++ cypress/e2e/api/requests/create_user.js
@@ -1,24 +1,24 @@
 import requestBodyUser from '../../../fixtures/user.json'
 
-
 export const createUserRequest = (email, password) => {
-
   const userWithDynamicEmail = {
     ...requestBodyUser,
     email: email,
-    password: password
+    password: password,
   }
 
-  return cy.request({
-    method: 'POST',
-    url: 'usuarios',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
-    body: userWithDynamicEmail,
-  }).then((response) => {
-    const userId = response.body._id
-    cy.wrap(userId).as('userId') 
-  })
+  return cy
+    .request({
+      method: 'POST',
+      url: 'usuarios',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: userWithDynamicEmail,
+    })
+    .then((response) => {
+      const userId = response.body._id
+      cy.wrap(userId).as('userId')
+    })
 }
