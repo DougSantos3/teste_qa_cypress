38292e159d6961fc3c0b0e8ed8a569fd5df4b36b
douglas <douglasmartinssantos7@gmail.com>
2024-12-10 12:18:46 -0300

    refactor: improve function parameter formatting for readability

--- cypress/e2e/api/requests/create_card.js
+++ cypress/e2e/api/requests/create_card.js
@@ -1,23 +1,24 @@
 export const createCardRequest = (authorization, productId) => {
-  
-  return cy.request({
-    method: 'POST',
-    url: 'carrinhos',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      Authorization: authorization,
-    },
-    body: {
-      produtos: [
-        {
-          idProduto: productId,
-          quantidade: 1,
-        },
-      ],
-    },
-  }).then((response) => {
-    const cardId = response.body._id  
-    cy.wrap(cardId).as('cardId')
-  })
+  return cy
+    .request({
+      method: 'POST',
+      url: 'carrinhos',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: authorization,
+      },
+      body: {
+        produtos: [
+          {
+            idProduto: productId,
+            quantidade: 1,
+          },
+        ],
+      },
+    })
+    .then((response) => {
+      const cardId = response.body._id
+      cy.wrap(cardId).as('cardId')
+    })
 }
