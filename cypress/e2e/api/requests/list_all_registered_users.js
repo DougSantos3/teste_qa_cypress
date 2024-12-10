12488abc31ec0439ec77c6dde095d65f323d750f
douglas <douglasmartinssantos7@gmail.com>
2024-12-10 12:20:43 -0300

    refactor: improve function parameter formatting for readability

--- cypress/e2e/api/requests/list_all_registered_users.js
+++ cypress/e2e/api/requests/list_all_registered_users.js
@@ -1,19 +1,22 @@
-export const listAllRegisteredUsers = (email, password, administrador = true) => {
-  
+export const listAllRegisteredUsers = (
+  email,
+  password,
+  administrador = true
+) => {
   const queryParams = {
-    _id: "7",
-    nome: "Teste Souza Silva",
+    _id: '7',
+    nome: 'Teste Souza Silva',
     email: email,
     password: password,
     administrador: administrador,
   }
 
   return cy.request({
-    method: "GET",
-    url: "usuarios",
+    method: 'GET',
+    url: 'usuarios',
     qs: queryParams,
     headers: {
-      Accept: "application/json",
+      Accept: 'application/json',
     },
   })
 }
