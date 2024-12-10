b537466f1a1b78c4fae3205a62e9079db771ee0a
douglas <douglasmartinssantos7@gmail.com>
2024-12-10 12:21:12 -0300

    refactor: improve function parameter formatting for readability

--- cypress/e2e/api/requests/search_user.js
+++ cypress/e2e/api/requests/search_user.js
@@ -1,11 +1,9 @@
 export const searchProduct = (user_id) => {
- 
-    cy.request({
-        method: "GET",
-        url: `usuarios/${user_id}`,
-        headers: {
-          Accept: "application/json",
-        },
-    })
-  }
-  
+  cy.request({
+    method: 'GET',
+    url: `usuarios/${user_id}`,
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+}
