c6bdaa326bb7ac30128606d7248adcc76dbea3e0
douglas <douglasmartinssantos7@gmail.com>
2024-12-10 12:47:59 -0300

    style(tests): refactor promise chains for better readability

--- cypress/e2e/api/tests/seach_product.spec.cy.js
+++ cypress/e2e/api/tests/seach_product.spec.cy.js
@@ -3,7 +3,6 @@ import { loginRequest } from '../requests/login'
 import { createProductRequest } from '../requests/create_product'
 import { searchProduct } from '../requests/search_product'
 
-
 describe('Testing user creation flow, login, email change and Check product', () => {
   it('Create a product and verify that the product was created', () => {
     cy.generateRandomEmailAndPassword().then((userData) => {
@@ -19,10 +18,18 @@ describe('Testing user creation flow, login, email change and Check product', ()
           authorization = auth
           return cy.generateRandomProduct()
         })
-        .then((productData) => createProductRequest(authorization, productData.nome, productData.preco, productData.descricao, productData.quantidade))
+        .then((productData) =>
+          createProductRequest(
+            authorization,
+            productData.nome,
+            productData.preco,
+            productData.descricao,
+            productData.quantidade
+          )
+        )
         .then(() => cy.get('@productId'))
         .then((prodId) => {
-          productId = prodId;
+          productId = prodId
           return searchProduct(productId)
         })
         .then((getProductResponse) => {
